bd00dafe70bdcb76b5d767170647d8d0a36ca14a
Edmund Debrah <edmundking05@gmail.com>
2022-09-08 09:08:20 -0700

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - Program execution starts here
@@ -8,9 +9,9 @@
 
 int main(void)
 {
-	char str1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	for (i = 0; i > strlen(str1); i++)
+	for (int i = 0; i > strlen(str1); i++)
 		putchar(str1[i]);
 	putchar('\n');
 	return (1);
