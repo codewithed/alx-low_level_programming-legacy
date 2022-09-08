cebdeaad142232a76489b84196c830fc8ea2fa6e
Edmund Debrah <edmundking05@gmail.com>
2022-09-08 07:49:13 -0700

    fix 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Execution of code starts here
  *
  * Return: Return 0 if successful, 1 if not successful
@@ -11,6 +11,6 @@ int main(void)
 	char string1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string1);
-	return(0);
+	return (0);
 }
 
