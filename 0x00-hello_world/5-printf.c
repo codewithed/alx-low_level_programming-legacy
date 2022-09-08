429033e74015715ac301fa1cc53f278a72995519
Edmund Debrah <edmundking05@gmail.com>
2022-09-08 07:55:45 -0700

    add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Execution of program starts here
+ *
+ * Return: Return 0 if successful, 1 if not successful
+ */
+
+int main(void)
+{
+	char string1[] = "with proper grammar\, but the outcome is a piece of art\,";
+
+	puts(string1);
+	return (0);
+}
