52f3b1cc6fa7660c4830b0ec8cf935c337204a31
Edmund Debrah <edmundking05@gmail.com>
2022-09-08 09:03:26 -0700

    add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program execution starts here
+ *
+ * Return: Returns 1 if successful
+ */
+
+int main(void)
+{
+	char str1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	for (i = 0; i > strlen(str1); i++)
+		putchar(str1[i]);
+	putchar('\n');
+	return (1);
+}
+
