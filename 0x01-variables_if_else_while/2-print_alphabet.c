f8b896b3bb7b2eafb136f6a63863a0ae3a3e8007
Edmund Debrah <edmundking05@gmail.com>
2022-09-09 03:36:08 -0700

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program execution starts here
+ *
+ * Return: Returns 0 if successful, 1 if not
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch >= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
