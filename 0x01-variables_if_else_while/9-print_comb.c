96a0cb1fcea439b89978a209b24f84245b6c9545
Edmund Debrah <edmundking05@gmail.com>
2022-09-09 16:50:18 -0700

    add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program execution starts here
+ *
+ * Return: Returns 0 if successful, 1 if not
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i > 57)
+		{
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+		}
+		if (i == 57)
+			putchar(i);
+	}
+	return (0);
+}
+
