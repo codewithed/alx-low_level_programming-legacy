b23089a67df3f4980356320427be2626dc7dd9c1
Edmund Debrah <edmundking05@gmail.com>
2022-09-09 04:51:48 -0700

    add 5-print_numbers.c'

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program execution starts here
+ *
+ * Return: Returns 0 if successful, 1 if not
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
