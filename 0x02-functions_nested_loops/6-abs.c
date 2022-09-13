ae1b835b76c955c4d23539004fde16034d55a56e
Edmund Debrah <edmundking05@gmail.com>
2022-09-13 09:14:31 -0700

    add 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * _abs - gives the absolute value
+ * @r: an int
+ *
+ * Return: 0 if successful
+ */
+
+int _abs(int r)
+{
+	if (r > 0)
+		_putchar(r);
+	if (r < 0)
+	{
+		r = r * -1;
+		_putchar(r);
+	}
+	return (0);
+}
+
