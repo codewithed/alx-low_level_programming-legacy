d7d5e43ba8a04527c396b8370434cd9ae785fab4
Edmund Debrah <edmundking05@gmail.com>
2022-09-13 04:39:53 -0700

    update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,11 +7,24 @@
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
+ *
+ * print_alphabet - Prints all the alphabet in lowercase followed by a new line
+ *
+ * Return: none
  */
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
 
 #endif
