41f6b1ee2c7e375377bb50f8647154a767d700b0
Edmund Debrah <edmundking05@gmail.com>
2022-09-13 04:47:12 -0700

    add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Program execution starts here
+ * Return: 0 if successful
+ *
+ * print_alphabet - Prints the whole alphabet
+ */
+
+int main(void)
+{
+	print_alphabet;
+	return (0);
+}
+
