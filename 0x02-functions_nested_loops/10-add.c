797e0d56a5b5a9e3bb65397befc3aa48b95f2223
Edmund Debrah <edmundking05@gmail.com>
2022-09-13 10:58:50 -0700

    add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.c"
+
+/**
+ * add - adds two numbers
+ * @a: an int
+ * @b: an int
+ *
+ * Return: the sum of two numbers
+ */
+
+int add(a, b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -24,3 +24,4 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int r);
 int print_last_digit(int n);
+int add(a, b);
