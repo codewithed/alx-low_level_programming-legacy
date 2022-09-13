411f82bb0f9bb762711d4056ae9faca20d625b93
Edmund Debrah <edmundking05@gmail.com>
2022-09-13 08:44:30 -0700

    add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks if anumber is positive or negative
+ * @n: an int
+ *
+ * Return: 1 if positive, 0 if 0, -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	if (n == 0)
+		_putchar('0');
+		return (0);
+	if (n < 0)
+		_putchar('-');
+		return (-1);
+}
+
