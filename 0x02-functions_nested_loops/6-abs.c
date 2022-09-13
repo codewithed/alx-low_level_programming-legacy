6b48808b9fc151f472e5b830088ae300abfff2c4
Edmund Debrah <edmundking05@gmail.com>
2022-09-13 09:33:55 -0700

    fix 6-abs.c' git push

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,15 +7,10 @@
  * Return: 0 if successful
  */
 
-int _abs(int r)
+int _abs(int)
 {
-	if (r > 0)
-		_putchar(r);
 	if (r < 0)
-	{
-		r = r * -1;
-		_putchar(r);
-	}
-	return (0);
+		r = r * (-1);
+	return (r);
 }
 
