4eaf13bcebe7b5e4700337a43f96bca66c2e3f1f
Edmund Debrah <edmundking05@gmail.com>
2022-09-13 04:28:13 -0700

    add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+#include <string.h>
+
+/*
+ * main - Program excution starts here
+ *
+ * Return: Return 0 if successful, 1 if not
+ *
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int i, len;
+	len = strlen(str);
+
+	for (i = 0; i <= len; i++)
+		_putchar("%c", str[i]);
+	return (0);
+}
+
