aafb802ee26fecbdb5ecfe581febc39fa0110fb2
Edmund Debrah <edmundking05@gmail.com>
2022-09-13 14:07:44 -0700

    add 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,8 +19,8 @@ void print_to_98(int n)
 			if (n != 98)
 			{
 				printf("%d  ,", n);
-				n++;
 			}
+			n++;
 		}
 	}
 
@@ -33,8 +33,8 @@ void print_to_98(int n)
 			if (n != 98)
 			{
 				printf("%d ,", n);
-				n--;
 			}
+			n--;
 		}
 	}
 }
