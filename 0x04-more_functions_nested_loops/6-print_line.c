f9636d982bc9d972631fc46097583f0dd2ea151d
Edmund Debrah <edmundking05@gmail.com>
2022-09-15 08:26:44 -0700

    fix 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,13 @@
 void print_line(int n)
 {
 	int i;
-	char line[100];
 
-	for (i = 0; i > n; i++)
+	if (n > 0)
 	{
-		line[i] = '_';
-		_putchar(line[i]);
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
