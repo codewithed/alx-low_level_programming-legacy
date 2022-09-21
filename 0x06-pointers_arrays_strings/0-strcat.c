69924b99df33472ae7bdc94b84f135bb1db37a7e
Edmund Debrah <edmundking05@gmail.com>
2022-09-21 08:16:42 -0700

    fix 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 
 /**
  * *_strcat - concatenates two strings
- * @*dest: dest pointer
+ * @dest: dest pointer
  * @src: src pointer
  *
  * Return: pointer to dest
@@ -10,17 +10,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i,j;
+	int i, j;
 
-	for(i = 0; dest[i] != '\0'; i++);
-
-	for(j = 0; src[j] != '\0'; j++)
+	for (i = 0; dest[i] != '\0'; i++);
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
 	}
 
 	dest[i + j] = '\0';
-
-	return dest;
+	return (dest);
 }
 
