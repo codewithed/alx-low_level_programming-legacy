339bc25a2d0abaa4731f965ad6077ae5353ef72c
Edmund Debrah <edmundking05@gmail.com>
2022-09-29 08:35:18 -0700

    add 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,8 +9,7 @@
 void _puts_recursion(char *s)
 {
 	int i = 0;
-
-	if (*(s + i) == "")
+	if (*(s + i) == '\0')
 	{
 		_putchar('\n');
 		return;
@@ -18,5 +17,6 @@ void _puts_recursion(char *s)
 
 	_putchar(*(s + i));
 	i++;
+	_puts_recursion(s + i);
 }
 
