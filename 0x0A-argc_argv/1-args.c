8e4fa08b72487bafc880edf6be3f6cab298a779d
Edmund Debrah <edmundking05@gmail.com>
2022-09-30 06:34:26 -0700

    add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program execution starts here
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0 if successful
+ */
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d", argc);
+	putchar('\n');
+	return (0);
+}
+
