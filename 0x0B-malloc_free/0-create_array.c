8ee21dce616c693b0eb577ac1452584972537fee
Edmund Debrah <edmundking05@gmail.com>
2022-10-04 08:58:01 -0700

    fix 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,10 +16,9 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-
 	arr = (char *)malloc(sizeof(char) * size);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i =! '\0'; i++)
 	{
 		*(arr + i) = c;
 	}
