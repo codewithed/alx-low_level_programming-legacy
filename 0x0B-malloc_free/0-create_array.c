8863f5664b928a233bd15a5391c906ec79d811b9
Edmund Debrah <edmundking05@gmail.com>
2022-10-04 05:46:59 -0700

    add 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an array given a size
+ * @size: size of array to be created
+ * @c: the car to be initialized
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = (char *)malloc(sizeof(char) * size);
+
+	*(arr + 0) = c;
+	return (arr);
+}
+
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Programs to practice memory management
