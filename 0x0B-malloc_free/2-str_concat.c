1fca200edceb1c4fa9258644b36526c6afb98cf7
Edmund Debrah <edmundking05@gmail.com>
2022-10-04 13:34:55 -0700

    fix 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,14 +22,14 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i <= s1_len; i++)
 	{
 		if (s1 == NULL)
-			*s1 = "";
+			*res = "";
 		res[i] = s1[i];
 	}
 
 	for (j = 0; j <= s2_len; j++)
 	{
 		if (s2 == NULL)
-			*s2 = "";
+			*res = "";
 		res[i + 1] = s2[j];
 		i++;
 	}
