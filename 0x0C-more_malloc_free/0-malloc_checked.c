de0201055313bab4ba8de039c6304456c701fa8d
“Edmund <edmundking05@gmail.com>
2022-10-06 09:59:46 -0700

    add 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to be allocated
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	else
+		return (ptr);
+}
+
