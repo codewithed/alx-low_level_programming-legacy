9febd907b44fbfb58540946e8aa7c359e2e291ce
“Edmund <edmundking05@gmail.com>
2022-10-10 22:35:02 -0700

    add 1-init_dog.c'

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,7 +5,7 @@
  * @d: pointer to structure
  * @name: pointer to the name of the dog
  * @age: age of dog
- * @owner: pointer to owner of dog       
+ * @owner: pointer to owner of dog
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
