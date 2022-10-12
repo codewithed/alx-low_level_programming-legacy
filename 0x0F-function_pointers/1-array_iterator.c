afb0bb152f504a4da91260ff7bdf9e636991b30e
“Edmund <edmundking05@gmail.com>
2022-10-12 06:40:12 -0700

    fix 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,10 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
-
-	for (i = 0; i < size; i++)
+	if (array && action)
 	{
-		action(array[i]);
+		while (size--)
+			action(*array++)
 	}
 }
 
