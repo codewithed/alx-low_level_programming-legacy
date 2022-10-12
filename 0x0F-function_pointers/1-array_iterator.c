a7f7decf40fa4b9f6aae1e15a725796d22c98e9c
“Edmund <edmundking05@gmail.com>
2022-10-12 06:04:01 -0700

    add 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - run a function as a parameter on each element of an array
+ * @array: pointer
+ * @size: the size of array
+ * @action: pointer to the function needed to be used
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	action(size);
+}
+
